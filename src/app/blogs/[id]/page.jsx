5d33d241f2d6b5f39e92d89cd64d3a604c11d859
Abu Saleh Md Siam <abusalehmdsiam@gmail.com>
2025-06-10 00:02:49 +0800

    blog details

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -1,14 +1,33 @@
 import dbConnect, { collectionNameObj } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import Image from "next/image";
 import React from "react";
+import { FaCommentDots } from "react-icons/fa";
+import { IoPersonSharp } from "react-icons/io5";
 
 export default async function BlogDetailsPage({ params }) {
   const p = await params;
   const blogCollection = dbConnect(collectionNameObj.blogCollection);
   const data = await blogCollection.findOne({ _id: new ObjectId(p.id) });
   return (
-    <div>
-      <p>{JSON.stringify(data)}</p>
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-8 p-4">
+        <Image src={data.image} width={800} height={500} alt={data.title} />
+        <div className="flex items-center gap-6">
+          <p className="flex items-center gap-2">
+            <FaCommentDots />
+            {data.comments.length} Comments
+          </p>
+          <p className="flex items-center gap-2">
+            <IoPersonSharp />
+            {data.author}
+          </p>
+        </div>
+        <p>{data.content}</p>
+      </div>
+      <div className="col-span-4 p-4">
+        <p className="text-xl ">Popular Blogs</p>
+      </div>
     </div>
   );
 }
